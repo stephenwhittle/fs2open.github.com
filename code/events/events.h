0606e16a5c3f1990b4340ce240b5b41856704d85
Stephen Whittle <swhittle@crypticwarrior.net>
2018-09-06 13:06:08 +1000

    Add first crop of HUD events

--- code/events/events.h
+++ code/events/events.h
@@ -13,4 +13,20 @@ extern util::event<void, int, int> GameLeaveState;
 extern util::event<void, int, int> GameEnterState;
 
 extern util::event<void, const char*> GameMissionLoad;
+
+extern util::event<void, int, int, bool> RenderWingman;
+
+extern util::event<void, float> RenderAfterburnerGauge;
+
+extern util::event<void, int, int> RenderMissionTime;
+
+extern util::event<void, float, float, float> RenderThrottleGauge;
+
+extern util::event<void, bool> RenderAutoTarget;
+
+extern util::event<void, bool> RenderAutoSpeed;
+extern util::event<void, int, const char*, const char*, bool> RenderPrimaryWeapon;
+extern util::event<void, int, const char*, const char*, bool, bool, int> RenderSecondaryWeapon;
+extern util::event<void, const char*, int, bool> RenderEscortShip;
+
 }
