6f03003042cf31162b3c64d4e357444b7e81ddc4
Stephen Whittle <swhittle@crypticwarrior.net>
2018-09-13 19:15:28 +1000

    add some extra pimpl notes

--- api/Monostate.h
+++ api/Monostate.h
@@ -25,3 +25,33 @@ private:
   static std::shared_ptr< abstract_base > mp;
 };
 std::shared_ptr< abstract_base > mono_nvi::mp;
+/*
+My last article in Overload [Schmidt15] generated a response from Szymon Gatner. Szymon suggested that all of the functions duplicated from the abstract base class to the Mono NVI class could be replaced with an overloaded pointer operator.
+
+The set of functions represented by foo() from listing 3 (page 11), implemented like this:
+
+  inline void foo ()
+  {
+    mp->foo (); // call to the virtual function
+                // in the concrete class
+  }
+could be replaced with one pointer operator overload function
+
+  T* operator-> ()
+  {
+    return  mp.get ();
+  }
+and used something like this instead of the method used in Listings 5 and 11 (pages 11 and 13):
+
+  typedef mono_nvi_template< abstract_base >
+    mono_nvi;
+  void nested_function ( void )
+  {
+    mono_nvi  mnvi;  // assumes its non-default
+                 // constructor has 
+    mnvi->foo (); // been called
+  }
+My first impression focused on the unusual way that the pointer operator was being used with a non-pointer variable. Szymon pointed out that “for classes of pointer semantics I think it is something C++ are used to by now. We already have unique_ptr and shared_ptr (and auto_ptr for a long time), also iterators have pointer semantics.”
+
+I encouraged Szymon to write up his idea for Overload. (Fran is always looking for good material; sometimes she has to settle for mine.) He said he would be “happy if you would like to side-note it in your next article”. Here it is, Szymon. Thanks, Bob
+*/
