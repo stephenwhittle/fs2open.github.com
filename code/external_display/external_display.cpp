84a9ed08c64374d14b798e9f66741fba420b40e4
Stephen Whittle <swhittle@crypticwarrior.net>
2018-09-19 09:07:34 +1000

    Countermeasure Gauge Support

--- code/external_display/external_display.cpp
+++ code/external_display/external_display.cpp
@@ -8,12 +8,13 @@ namespace external_display
 {
 	void OnRenderCountermeasureGauge(int NumberOfCountermeasures);
 	void OnRenderPrimaryWeapon(int WeaponIndex, const char* WeaponName, int CurrentAmmo, int MaxAmmo, bool Linked);
-
+	void OnRenderLockWarning(int Warning);
 	enum class ReportID
 	{
+		REPORT_COUNTERMEASURES = 2,
 		REPORT_PRIMARYWEAPON = 1
 	};
-
+	static bool ShowLockWarning = false;
 	hid_device* CurrentDevice = nullptr;
 	
 	void init() 
@@ -22,10 +23,17 @@ namespace external_display
 		CurrentDevice = hid_open(1155, 22352, nullptr);
 		events::RenderPrimaryWeapon.add(OnRenderPrimaryWeapon);
 		events::RenderCountermeasureGauge.add(OnRenderCountermeasureGauge); 
+		events::RenderLockWarning.add(OnRenderLockWarning);
+	}
+
+	void OnRenderLockWarning(int Warning)
+	{
+		ShowLockWarning = (Warning != 0);
 	}
 
 	uint8_t ReportBuffer[21] = {0};
 
+	
 void OnRenderPrimaryWeapon(int WeaponIndex, const char* WeaponName,int CurrentAmmo, int MaxAmmo, bool Linked )
 {
 	for (uint8_t ReportByte : ReportBuffer)
@@ -55,7 +63,38 @@ void OnRenderPrimaryWeapon(int WeaponIndex, const char* WeaponName,int CurrentAm
 }
 void OnRenderCountermeasureGauge(int NumberOfCountermeasures)
 {
-	outwnd_printf2("The number of countermeasures is %d\n", NumberOfCountermeasures);
+	static uint8_t Number[] = { 
+	0b0111111,
+	0b0000110,
+	0b1011011,
+	0b1001111,
+	0b1100110,
+	0b1101101,
+	0b1111101,
+	0b0000111,
+	0b1111111,
+	0b1101111,
+	};
+
+	uint8_t ReportBuffer[5] = { 0 };
+	ReportBuffer[0] = (uint8_t)ReportID::REPORT_COUNTERMEASURES;
+	if (ShowLockWarning)
+	{
+		ReportBuffer[1] = 0b0111000;
+		ReportBuffer[2] = 0b1011100;
+		ReportBuffer[3] = 0b0111001;
+		ReportBuffer[4] = 0b1110101;
+	}
+	else 
+	{
+		ReportBuffer[1] = 0b0111001;
+		ReportBuffer[2] = 0b1101101;
+		ReportBuffer[3] = Number[(NumberOfCountermeasures / 10) % 10];
+		ReportBuffer[4] = Number[NumberOfCountermeasures % 10];
+	}
+
+	hid_write(CurrentDevice, ReportBuffer, 5);
+
 }
 
 } // namespace external_display
