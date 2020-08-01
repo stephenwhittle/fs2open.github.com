ddbdcd5aa6446f08b467e4aa0fd809f964869968
Stephen Whittle <swhittle@crypticwarrior.net>
2020-08-01 19:28:15 +1000

    Add Color module implementation

---
+++ code/color/color/SCPColorModule.cpp
@@ -0,0 +1,30 @@
+#include "FSStdTypes.h"
+#include "cfile/SCPCFileModule.h"
+#include "color/SCPColorModule.h"
+#include "module/SCPModuleManager.h"
+#include "parse/SCPParser.h"
+
+
+bool SCPColorModule::StartupModule() 
+{
+	Colors = std::make_unique<SCPColorSet>(SCPColorSet::DefaultColors());
+	auto CFileModule = SCPModuleManager::GetModule<SCPCFileModule>();
+	Assert(CFileModule);
+	auto ColorsTable = CFileModule->FindFileInfo("colors.tbl", SCPCFilePathTypeID::Tables);
+	if (ColorsTable.has_value()) {
+		auto RootTable                  = CFileModule->CFileOpen(*ColorsTable, {SCPCFileMode::Read});
+		SCP_buffer RootColorTableBuffer = RootTable->ReadAllContentsIntoBuffer();
+		SCPParser::LoadIntoTable(*Colors, ColorsFile, std::move(RootColorTableBuffer), "colors.tbl");
+	}
+
+	FileFilter ColorTableFilter;
+	ColorTableFilter.FilenameMatchesRegex("*-clr.tbm");
+	ColorTableFilter.PathTypeIs(SCPCFilePathTypeID::Tables);
+	for (SCPCFileInfo FileInfo : CFileModule->CFileDatabase().Files(ColorTableFilter)) {
+		auto Table = CFileModule->CFileOpen(FileInfo, {SCPCFileMode::Read});
+		if (Table) {
+			SCP_buffer TableBuffer = Table->ReadAllContentsIntoBuffer();
+			SCPParser::LoadIntoTable(*Colors, ColorsFile, std::move(TableBuffer), FileInfo.GetFileName());
+		}
+	}
+}
---
+++ code/color/color/SCPColorModule.h
@@ -0,0 +1,20 @@
+#pragma once
+
+#include "module/SCPModuleBase.h"
+
+class SCPColorModule : public SCPModule<SCPColorModule>
+{
+
+public:
+	bool StartupModule() override;
+
+	void ShutdownModule() override {}
+	
+	std::unique_ptr<SCPColorSet> Colors;
+
+	static SCPColorModule ConstructModule(class SCPCFileModule&)
+	{
+		SCPColorModule Module;
+		return Module;
+	}
+};
